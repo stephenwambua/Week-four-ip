d83b75782885d4046e03fac32897ee8260d461c4
stephen wambua <ngumbausteph@gmail.com>
2019-11-10 07:29:19 +0300

    add a method onclick to window

--- js/script.js
+++ js/script.js
@@ -7,3 +7,8 @@ btn.onclick = function(){
 span.onclick = function(){
     modal.style.display = "none";
 }
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+}
