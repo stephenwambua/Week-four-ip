8f37d94037e241dcf5eb938b00d42672b30a21eb
stephen wambua <ngumbausteph@gmail.com>
2019-11-12 10:46:20 +0300

    add prompt and alert in js and confirmation button in html

--- js/script.js
+++ js/script.js
@@ -24,3 +24,20 @@ $("#confirm").click(function() {
    cell4.innerHTML = pizzaNumber;
    cell5.innerHTML = totalCosts;
 });
+var deliverCost = 250;
+btn.onclick = function() {
+  var chekingOut= prompt("Would you like your pizza delivered? if yes please state where.");
+  if (chekingOut != null) {
+    alert("Your delivery fee is 250/=")
+    alert(`your total amount is ${totalCosts + deliverCost}`);
+    alert("You will receive your order in 30 minutes....."); 
+  }
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
