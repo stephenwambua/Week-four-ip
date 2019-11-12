efa1eda6504766e8b6eb2533c1766f800eaee626
stephen wambua <ngumbausteph@gmail.com>
2019-11-12 08:55:49 +0300

    add table to the html js and html to show data

--- js/script.js
+++ js/script.js
@@ -1,61 +1,26 @@
 // Check out button event handling
-$( "#checkout" ).click(function() {
-    var pizza= parseInt($( "#psizes option:selected" ).val(),10);  
-    console.log(typeof(pizza))
+$("#confirm").click(function() {
+    // var pizzaSize = document.getElementById("psizes").text;
+    var pizzaSize=$( "#psizes option:selected" ).text();
+    var pizzaToppings=$("#pTopping option:selected").text();
+    var pizzaCrusts =$("#pcrusts option:selected").text();  
+    var pizza= parseInt($( "#psizes option:selected" ).val(),10); 
     var crust = parseInt($("#pcrusts option:selected").val(),10); 
-    var ptoppings = parseInt($("#pTopping option:selected").val(),10); 
+    var ptoppings = parseInt($("#pTopping option:selected").val(),10);
     var pizzaNumber = parseInt($("#pizzano").val(),10); 
-    var Delivery = parseInt($("#delivery").val(),10);
-    totalCosts=((pizza+crust+ptoppings)*pizzaNumber)+Delivery;
-   alert("totoal is " + totalCosts+"And the delivery fee is:"+Delivery);
-  
-})
-
-// Checkout end
-
-let modal = document.getElementById("myModal");
-let span = document.getElementsByClassName("close")[0];
-let btn = document.getElementById("myBtn");
-let btnCheckout = document.getElementById("checkout");
-btnCheckout.onclick = function(){
-
-}
-btn.onclick = function(){
-    modal.style.display = "block";
-}
-span.onclick = function(){
-    modal.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-}
-
-// Pizza Sizes
-function pizzaSizes(large, medium, small){
-    this.large =large;
-    this.medium=medium;
-    this.small=small;
-}
-let newPizza= new pizzaSizes(1200, 1000, 1500, 1300)
-
-// Pizza Crusts
-function pizzaCrust(crispy, stuffed, gluttenFree){
-    this.crispy = crispy;
-    this.stuffed = stuffed;
-    this.gluttenFree = gluttenFree;
-}
-let newCrust= new pizzaCrust(200, 100, 150, 130)
-
-// Pizza Toppings
-function pizzaTopping(Cheese, Pepperoni, Meat, Veggies){
-    this.Cheese = Cheese;
-    this.Pepperoni = Pepperoni;
-    this.Meat = Meat;
-    this.Veggies = Veggies;
-}
-let newTopping= new pizzaTopping(1200, 1000, 1500, 1300)
-
-
-
+    var tbl = document.getElementById("myTable");
+    var row = tbl.insertRow();
+    var cell1 = row.insertCell();
+    var cell2 = row.insertCell(); 
+    var cell3 = row.insertCell(); 
+    var cell4 = row.insertCell();
+    var cell5 = row.insertCell(); 
+    // var Delivery = parseInt($("#delivery").val(),10);
+    totalCosts=(pizza+crust+ptoppings)*pizzaNumber;
+//    alert("Total is " + totalCosts);
+   cell1.innerHTML = pizzaSize;
+   cell2.innerHTML = pizzaToppings;
+   cell3.innerHTML = pizzaCrusts;
+   cell4.innerHTML = pizzaNumber;
+   cell5.innerHTML = totalCosts;
+});
